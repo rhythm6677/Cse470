f2212c1c390d1df4815d5322e87ecd5db2793ec4
rhythm6677 <rhythm6677@gmail.com>
2025-05-09 23:30:34 +0600

    sprint 3

--- rest-mng-app-react-main/src/components/Progress.jsx
+++
@@ -1,7 +0,0 @@
-export default function ProgressPage() {
-    return (
-        <div className="w-screen h-screen flex align-center justify-center">
-            <span className="loading loading-infinity loading-lg"></span>
-        </div>
-    );
-}
